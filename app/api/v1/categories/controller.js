24898ef7a0f68df8c6f35b7359f81a09db61f4db
ringinrp <ringinrp21@gmail.com>
2023-12-04 12:41:59 +0700

    update setup

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -62,16 +62,22 @@ const update = async (req, res, next) => {
             name
         } = req.body;
 
-
         const result = await Categories.findOneAndUpdate({
-            _id: id,
+            _id: id
         }, {
             name
         }, {
             new: true,
             runValidators: true
         });
+        if (!result) {
+            return res.status(400).json({
+                message: 'Id categories tidak ditemukan!!'
+            });
+        }
 
+        result.name = name
+        result.save();
         res.status(200).json({
             data: result,
         });
@@ -81,17 +87,27 @@ const update = async (req, res, next) => {
     }
 }
 
-const destroy = async (req, res, next)=>{
+const destroy = async (req, res, next) => {
     try {
-        
-    } catch (error) {
-        
+        const {
+            id
+        } = req.params;
+        const result = await Categories.findByIdAndDelete({
+            _id: id
+        });
+
+        res.status(200).json({
+            data: result,
+        });
+    } catch (err) {
+        next(err);
     }
 }
 
 module.exports = {
     index,
     find,
+    destroy,
     update,
     create,
 }
--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -4,12 +4,14 @@ const {
     create,
     index,
     find,
-    update
+    update,
+    destroy
 } = require('./controller')
 
 router.get('/categories', index);
 router.get('/categories/:id', find);
 router.put('/categories/:id', update);
+router.delete('/categories/:id', destroy);
 
 router.post('/categories', create);
 
